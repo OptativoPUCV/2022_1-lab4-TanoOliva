449ca26343836c83ee44fed6323d12f128e39e36
Your Name <you@example.com>
2022-04-10 11:12:36 -0400

    new try

--- hashmap.c
+++ hashmap.c
@@ -132,10 +132,11 @@ Pair * searchMap(HashMap * map,  char * key) {
 
 Pair * firstMap(HashMap * map) {
     for (long idx=0; idx<map->capacity;idx++){
-        if (map->buckets[idx]->key != NULL && map->buckets[idx]->value != NULL) {
+        if ((map->buckets[idx]->key != NULL) && (map->buckets[idx]->value != NULL)) {
             map->current=idx;
             return map->buckets[idx];
         }
+
     }
     //si retorna null significa que no habia ningun elemento en el arrgelo 
     return NULL;
@@ -143,7 +144,7 @@ Pair * firstMap(HashMap * map) {
 
 Pair * nextMap(HashMap * map) {
     for (long idx=map->current; idx<map->capacity;idx++){
-        if (map->buckets[idx]->key != NULL && map->buckets[idx]->value != NULL) {
+        if ((map->buckets[idx]->key != NULL) && (map->buckets[idx]->value != NULL)) {
             map->current=idx;
             return map->buckets[idx];
         }
